a4737f8d8a44b6065af6ff7356fe04eff6e3103f
charankumar <kumarcharan2017@gmail.com>
2020-05-21 19:36:31 +0530

    Small changes

--- src/testRun/Test6.java
+++ src/testRun/Test6.java
@@ -8,7 +8,7 @@ public class Test6 extends basePage {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
+		//Some medium changes
 
 		basePage.initalize();
 
