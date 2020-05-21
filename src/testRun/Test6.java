5c55af2860388c57c3768419aeec6a12383d48b2
charankumar <kumarcharan2017@gmail.com>
2020-05-21 19:41:49 +0530

    Dummy changes

--- src/testRun/Test6.java
+++ src/testRun/Test6.java
@@ -9,6 +9,8 @@ public class Test6 extends basePage {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		//Some medium changes
+		
+		//Irritating
 
 		basePage.initalize();
 
